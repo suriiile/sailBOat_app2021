2256d4f52c1c9acc6739c1889a8bd01023550e6f
Suri Le <surilephuong2007@gmail.com>
2021-08-19 12:02:29 -0700

    layout+did a few buttons

---
+++ sailBOat_app/ProgramsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProgramsViewController.swift
+//  sailBOat_app
+//
+//  Created by Suri Le on 8/19/21.
+//
+
+import UIKit
+
+class ProgramsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
